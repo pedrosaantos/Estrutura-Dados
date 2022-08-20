85f0ea8f02426d901629583bf0f8d6a72db19b79
Pedro dos Santos <104872899+pedrosaantos@users.noreply.github.com>
2022-08-20 12:26:04 -0300

    Add files via upload

---
+++ Aula 1 C/aula1.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    float nota1, nota2, media;
+
+    printf("Nota 1: ");
+    scanf("%f", &nota1);
+
+    printf("Nota 2: ");
+    scanf("%f", &nota2);
+
+    media=(nota1+nota2)/2;
+
+    printf ("Sua me,dia foi %f", media);
+
+
+}
